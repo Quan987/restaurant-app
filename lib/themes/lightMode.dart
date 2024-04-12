220f812fb532ee16082fa0fcc8fcac50deb3a5d6
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2024-04-12 12:42:33 -0400

    Added themes

---
+++ lib/themes/lightMode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Color.fromARGB(255, 254, 254, 250),
+    primary: Color.fromARGB(255, 88, 41, 0),
+    secondary: Color.fromARGB(255, 255, 211, 161),
+    tertiary: Color.fromARGB(255, 174, 68, 12),
+    inversePrimary: Colors.grey.shade900,
+  )
+);
