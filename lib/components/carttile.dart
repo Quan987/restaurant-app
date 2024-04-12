66ab6a6fda90290ee6e61c26d619898685c7adff
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2024-04-12 12:40:01 -0400

    Added components

---
+++ lib/components/carttile.dart
@@ -0,0 +1,100 @@
+import 'package:testproject2/components/quantityselector.dart';
+import 'package:testproject2/models/cartitem.dart';
+import 'package:testproject2/models/restaurant.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyCartTile extends StatelessWidget {
+  final CartItem cartItem;
+
+  const MyCartTile({super.key, required this.cartItem});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<Restaurant>(
+      builder: (context, restaurant, child) => Container(
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.secondary,
+            borderRadius: BorderRadius.circular(16)),
+        margin: EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Image.asset(
+                    cartItem.food.imagePath,
+                    height: 70,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 11.0, bottom: 8, left: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(cartItem.food.name),
+                      Text("\$${cartItem.food.price}"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      MyQuantitySelector(
+                        quantity: cartItem.quantity,
+                        food: cartItem.food,
+                        onIncrement: () {
+                          restaurant.addToCart(
+                              cartItem.food, cartItem.selectedAddons);
+                        },
+                        onDecrement: () {
+                          restaurant.removeFromCart(cartItem);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            // Addons
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0, bottom: 8),
+              child: SizedBox(
+                height: cartItem.selectedAddons.isEmpty ? 0 : 60,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: cartItem.selectedAddons
+                      .map((addon) => Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: FilterChip(
+                              label: Row(
+                                children: [
+                                  Text(addon.name),
+                                  SizedBox(width: 16),
+                                  Text("(\$${addon.price})"),
+                                ],
+                              ),
+                              shape: StadiumBorder(
+                                side: BorderSide(
+                                  color: Theme.of(context).colorScheme.tertiary,
+                                ),
+                              ),
+                              onSelected: (value) {},
+                              backgroundColor:
+                                  Theme.of(context).colorScheme.secondary,
+                              labelStyle: TextStyle(
+                                color: Theme.of(context).colorScheme.tertiary,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ))
+                      .toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/silverappbar.dart
@@ -0,0 +1,70 @@
+import 'package:testproject2/pages/cart.dart';
+import 'package:flutter/material.dart';
+
+class MySilverAppbar extends StatelessWidget {
+  final Widget child;
+  final Widget title;
+  const MySilverAppbar({super.key, required this.child, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 340,
+      collapsedHeight: 120,
+      floating: false,
+      pinned: true,
+      actions: [
+        IconButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CartPage(),
+              ),
+            );
+          },
+          icon: Icon(Icons.shopping_cart),
+        )
+      ],
+      backgroundColor: Theme.of(context).colorScheme.background,
+      foregroundColor: Theme.of(context).colorScheme.primary,
+      title: Text('Food Delivery Application'),
+      centerTitle: true,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: const EdgeInsets.only(bottom: 50.0),
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              child,
+              Positioned(
+                bottom: -20,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: 40,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Theme.of(context).colorScheme.background,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        title: title,
+        centerTitle: true,
+        titlePadding: EdgeInsets.only(left: 0, right: 0),
+        expandedTitleScale: 1,
+      ),
+    );
+  }
+}
