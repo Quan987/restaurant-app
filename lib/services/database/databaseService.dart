a4790f5d01e91f6fc8c13f05b12906835ae17db5
QuanT <88959861+Quan987@users.noreply.github.com>
2024-04-16 17:42:39 -0400

    Update database file & method for registering user

---
+++ lib/services/database/databaseService.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class FirestoreService {
+  // final CollectionReference orders = FirebaseFirestore.instance.collection('orders');
+
+  // Future<void> saveOrderToDatabase(String receipt) async {
+  //   await orders.add({
+  //     'data' : DateTime.now(),
+  //     'order': receipt,
+  //   });
+  // }
+
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+
+  FirebaseFirestore getDatabase() {
+    return _db;
+  }
+
+  CollectionReference getDBCollection(String collection) {
+    return _db.collection(collection);
+  }
+
+  Future<void> dbUserRegister(
+      Map<String, String> userinfo, String collection) async {
+    try {
+      CollectionReference db = _db.collection(collection);
+      await db.doc().set({
+        "fname": userinfo["fname"],
+        "lname": userinfo["lname"],
+        "email": userinfo["email"],
+        "password": userinfo["password"],
+        "time": userinfo["timestamp"],
+        "id": userinfo["id"],
+      });
+    } on FirebaseException catch (e) {
+      throw Exception(e.code);
+    }
+  }
+}
