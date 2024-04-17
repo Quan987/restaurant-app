824050008c623d80748ea4cc1bbbad12cf847e00
QuanT <88959861+Quan987@users.noreply.github.com>
2024-04-17 16:19:23 -0400

    Commented all codes in food.dart

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,56 +1,30 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-  final Function()? onTap;
-  final String text;
+  const MyButton({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
 
-  const MyButton({Key? key, required this.text, required this.onTap})
-      : super(key: key);
+  final String title;
+  final Function? onTap;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.all(20),
-        margin: EdgeInsets.symmetric(horizontal: 25),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(30),
-          gradient: RadialGradient(
-            colors: [
-              Colors.pink.shade400,
-              Colors.orange.shade300,
-              Colors.yellow.shade50,
-            ],
-            center: Alignment.topLeft,
-            radius: 1.5,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.3),
-              blurRadius: 10,
-              offset: Offset(0, 5),
-            ),
-          ],
-        ),
-        child: Center(
-          child: Text(
-            text,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.black.withOpacity(1.0),
-              shadows: [
-                Shadow(
-                  color: Colors.black.withOpacity(0.5),
-                  blurRadius: 5,
-                  offset: Offset(2, 2),
-                ),
-              ],
-            ),
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 2,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(8),
           ),
         ),
       ),
+      child: Text("Login".toUpperCase()),
     );
   }
 }
