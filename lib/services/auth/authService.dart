00b04080ad42728950aa6cbe2effea3ed88cb284
QuanT <88959861+Quan987@users.noreply.github.com>
2024-04-17 18:26:14 -0400

    Remove all try-catch from authService.dart method

--- lib/services/auth/authService.dart
+++ lib/services/auth/authService.dart
@@ -12,25 +12,17 @@ class AuthService {
   String get getCurrentUserUID => _auth.currentUser!.uid;
 
   Future<void> authUserRegister(String email, String password) async {
-    try {
-      await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-    } on FirebaseAuthException catch (e) {
-      throw Exception(e.code);
-    }
+    await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
   }
 
   Future<void> authUserLogin(String email, String password) async {
-    try {
-      await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-    } on FirebaseAuthException catch (e) {
-      throw Exception(e.code);
-    }
+    await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
   }
 
   Future<void> logout() async {
