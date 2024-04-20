da7330c4ed37db2c11ac18427db78817f07753ea
QuanT <88959861+Quan987@users.noreply.github.com>
2024-04-19 21:02:43 -0400

    Create custom button for setting page

---
+++ lib/components/setting_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class MySettingWidget extends StatelessWidget {
+  const MySettingWidget(
+      {super.key,
+      required this.title,
+      required this.subtitle,
+      required this.circle});
+
+  final String title;
+  final String subtitle;
+  final Color circle;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(20),
+      onTap: () {},
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          border: Border.all(width: 1),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  subtitle,
+                  style: TextStyle(fontSize: 16, color: Colors.grey[600]),
+                ),
+              ],
+            ),
+            CircleAvatar(
+              backgroundColor: circle,
+              radius: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
