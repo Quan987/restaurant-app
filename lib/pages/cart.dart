63d40417ecfb813f85d5b116f305470b3e5dfdcf
SophieNguyen113 <sophiengocnguyen113@gmail.com>
2024-04-25 21:52:07 -0400

    Fixxed Botton Navigator

--- lib/pages/cart.dart
+++ lib/pages/cart.dart
@@ -1,11 +1,30 @@
 import 'package:project2/components/button.dart';
 import 'package:project2/models/restaurant.dart';
 import 'package:project2/pages/home.dart';
+import 'package:project2/components/bottomnav.dart';
+import 'package:project2/components/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
   const CartPage({super.key});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,9 +35,9 @@ class CartPage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Cart',
             style: TextStyle(
-            color: Theme.of(context).colorScheme.primary,
-            fontWeight: FontWeight.bold,
-          ),),
+              color: Theme.of(context).colorScheme.primary,
+              fontWeight: FontWeight.bold,
+            ),),
           backgroundColor: Colors.transparent,
           foregroundColor: Theme.of(context).colorScheme.primary,
           centerTitle: true,
@@ -28,24 +47,24 @@ class CartPage extends StatelessWidget {
                 showDialog(
                     context: context,
                     builder: (context) => AlertDialog(
-                          title: Text(
-                            "Are you sure you want to clear the cart?",
-                            style: TextStyle(fontSize: 20),
-                          ),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: Text('Cancel'),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
-                                restaurant.clearCart();
-                              },
-                              child: Text('Yes'),
-                            ),
-                          ],
-                        ));
+                      title: Text(
+                        "Are you sure you want to clear the cart?",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          child: Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            restaurant.clearCart();
+                          },
+                          child: Text('Yes'),
+                        ),
+                      ],
+                    ));
               },
               icon: Icon(Icons.delete),
             ),
@@ -58,40 +77,34 @@ class CartPage extends StatelessWidget {
                 children: [
                   userCart.isEmpty
                       ? Expanded(
-                          child: Center(
-                            child: Text(
-                              "Cart is empty",
-                              style: TextStyle(
-                                  color:
-                                  Theme.of(context).colorScheme.primary),
-                            ),
-                          ),
-                        )
+                    child: Center(
+                      child: Text(
+                        "Cart is empty",
+                        style: TextStyle(
+                            color:
+                            Theme.of(context).colorScheme.primary),
+                      ),
+                    ),
+                  )
                       : Expanded(
-                          child: ListView.builder(
-                              itemCount: userCart.length,
-                              itemBuilder: (context, index) {
-                                final cartItem = userCart[index];
-                                // return MyCartTile(cartItem: cartItem);
-                                return HomePage();
-                              }),
-                        ),
+                    child: ListView.builder(
+                        itemCount: userCart.length,
+                        itemBuilder: (context, index) {
+                          final cartItem = userCart[index];
+                          // return MyCartTile(cartItem: cartItem);
+                          return HomePage();
+                        }),
+                  ),
                 ],
               ),
             ),
-            MyButton(
-              title: "Checkout",
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            )
           ],
         ),
+        bottomNavigationBar: const MyBottomNavBar(currentIndex: 3),
       );
     });
   }
 }
+
+
+
